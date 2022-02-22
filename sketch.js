65f9afeceaf0bebf4792be3f3b6338d3cc28ed21
SofiaBelezia <93799292+SofiaBelezia@users.noreply.github.com>
2022-02-22 18:56:17 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,79 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+var engine, world, backgroundImg;
+
+var canvas, angle, tower, ground, cannon;
+
+var balls=[]
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.gif");
+  towerImage = loadImage("./assets/tower.png");
+}
+
+function setup() {
+
+  canvas = createCanvas(1200, 600);
+  engine = Engine.create();
+  world = engine.world;
+  angleMode(DEGREES)
+  angle=15
+  
+  var options = {
+    isStatic: true
+  }
+
+  ground = Bodies.rectangle(0, height - 1, width * 2, 1, options);
+  World.add(world, ground);
+
+  tower = Bodies.rectangle(160, 350, 160, 310, options);
+  World.add(world, tower);
+
+  cannon=new Cannon(180,110,130,100,angle)
+
+}
+
+function draw() {
+  image(backgroundImg,0,0,1200,600)
+  Engine.update(engine);
+
+  
+  rect(ground.position.x, ground.position.y, width * 2, 1);
+  
+
+  push();
+  imageMode(CENTER);
+  image(towerImage,tower.position.x, tower.position.y, 160, 310);
+  pop();  
+
+for(var i =0;i<balls.lenght;i++){
+  showCannonBalls(balls[i])
+}
+
+  cannon.display()
+
+}
+
+function keyPressed(){
+  if(keyCode===DOWN_ARROW){
+    var CannonBall=new cannonBall(cannon.x,cannon.y)
+    CannonBall.trajectory=[]
+    Matter.Body.setAngle(CannonBall.body,cannon.angle)
+    balls.push(CannonBall)
+  }
+}
+
+function showCannonBalls(ball){
+  if(ball){
+    ball.display
+  }
+}
+
+function keyReleased(){
+  if (keyCode===DOWN_ARROW){
+    balls[balls.lenght-1].shoot()
+  }
+}
+
